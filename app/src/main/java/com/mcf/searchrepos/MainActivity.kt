a97ed6826eb8e001192464d5ad2526854121ef8c
Mateus Conte <mateusconte_13@hotmail.com>
2020-07-09 22:34:46 -0300

    Cell layout

--- app/src/main/java/com/mcf/searchrepos/MainActivity.kt
+++ app/src/main/java/com/mcf/searchrepos/MainActivity.kt
@@ -2,11 +2,12 @@ package com.mcf.searchrepos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.cell)
     }
 }
